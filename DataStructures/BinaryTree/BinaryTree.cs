10c753a7eb7ee45be1cfb2c70088050fe429c031
flashm123 <n_plk7a@mail.ru>
2021-04-21 19:57:46 +0300

    Add BinaryTree

---
+++ DataStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+        public Node root;
+        
+        public BinaryTree()
+        {
+        }
+
+        public BinaryTree(params int[] nodes)
+        {
+            foreach (var node in nodes)
+            {
+                Insert(ref root, node);
+            }
+        }
+
+        public void Insert(ref Node node, int data)
+        {
+            if (node == null)
+            {
+                var itemToAdd = new Node(data);
+                node = itemToAdd;
+            }
+            else
+            {
+                if (data < node.Data)
+                {
+                    Insert(ref node.Left, data);
+                }
+                else if (data >= node.Data)
+                {
+                    Insert(ref node.Right, data);
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/BinaryTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        public Node(int data)
+        {
+            Data = data;
+        }
+
+        public int Data
+        {
+            get;
+            set;
+        }
+
+        public Node Left;
+        public Node Right;
+        
+    }
+}
